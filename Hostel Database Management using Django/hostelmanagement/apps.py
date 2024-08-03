111d4891194da143df21cd8f0d6195458ec35c47
Manikumar <99137355+zabuzuba@users.noreply.github.com>
2024-08-03 21:01:19 +0530

    Delete Hostel Database Management using Django directory

--- Hostel Database Management using Django/hostelmanagement/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HostelmanagementConfig(AppConfig):
-    name = 'hostelmanagement'
--- Hostel Database Management using Django/selection/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SelectionConfig(AppConfig):
-    name = 'selection'
-    verbose_name = 'Hostel'
